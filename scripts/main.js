d91d92a481241c2bd6fb223d70c79b1918209b95
Talal Obeid <talal.obeid@gmail.com>
2014-08-07 13:04:07 -0400

    ran gulp build and generated dist

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,13 @@
-console.log('The Iron Yard Rocks');
+$(document).ready(function() {
+    var bodyheight = $(window).height();
+    $("header").height(bodyheight);
+});
+
+// for the window resize
+$(window).resize(function() {
+    var bodyheight = $(window).height();
+    $("header").height(bodyheight);
+});
+
+
+// screen.availHeight
